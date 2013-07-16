bb20717f6148773abd7943e1c0712759f21e6665
Emilia Kubo Kirschenbaum <ekubokirs@gmail.com>
2013-07-16 15:34:26 -0700

    altered sessions_controller_spec.rb

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,26 +1,15 @@
 require 'spec_helper'
 
 describe SessionsController do
+  subject(:user) { Fabricate(:user)}
 
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
-    end
+  describe "POST #create" do
+    it "logs an authenticated user in and redirects to site#index"
+
+    it "redisplays login form for unauthenticated users"
+
+    it "sets code and expires_at for a password reset request"
+
   end
-
-  describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'destroy'" do
-    it "returns http success" do
-      get 'destroy'
-      response.should be_success
-    end
-  end
-
+  
 end
